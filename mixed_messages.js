860d8ab0b30ff78a45a951317b05c492a20a90e9
jerevaisan <31984703+jerevaisan@users.noreply.github.com>
2020-11-09 11:24:12 +0200

    Finnished

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,1 +1,14 @@
-console.log("Hello!");
+const firsWordArr = ['You', 'We all', 'People'];
+const secondWordArr = ['have to', 'must', 'can'];
+const thirdWordArr = ['make', 'change', 'stop'];
+const fourthWordArr = ['the world', 'the space', 'this time'];
+const finalWordArr = ['here and now!', 'for the better!', 'to all people!']
+
+let firstWord = firsWordArr[Math.floor(Math.random()*3)];
+let secondWord = secondWordArr[Math.floor(Math.random()*3)];
+let thirdWord = thirdWordArr[Math.floor(Math.random()*3)];
+let fourthWord = fourthWordArr[Math.floor(Math.random()*3)];
+let finalWord = finalWordArr[Math.floor(Math.random()*3)];
+
+
+console.log(`${firstWord} ${secondWord} ${thirdWord} ${fourthWord} ${finalWord}`);
